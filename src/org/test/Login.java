efdeda9d1b6c62914bc050748d93c86f3bf71d5a
ShriLekha24 <shrilekha100@gmail.com>
2021-11-09 12:10:06 +0530

    Username functionality

--- src/org/test/Login.java
+++ src/org/test/Login.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Login {
+	
+	public static void username() {
+		System.out.println("Username");
+	}
+	
+	public static void main(String[] args) {
+		username();
+	}
 
 }
